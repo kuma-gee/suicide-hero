e09f964a430dba83f2a9c9acabb8e80b13794786
myin <minyin142p@gmail.com>
2021-08-28 17:29:47 +0200

    Use key for about page

--- src/scenes/menu/Menu.gd
+++ src/scenes/menu/Menu.gd
@@ -4,8 +4,6 @@ extends Control
 func _ready():
 	GUI.open_menu(GUI.Main, true)
 
-
-func _unhandled_input(_event):
-	if Input.is_action_pressed("ui_cancel") and Input.is_action_just_pressed("ui_accept"):
+func _unhandled_input(event: InputEvent):
+	if event.is_action_pressed("about"):
 		GUI.open_menu(GUI.About)
-		get_tree().set_input_as_handled()
