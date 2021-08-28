300ed70d34e32e6e8f94caa34e001e015d4db273
myin <minyin142p@gmail.com>
2021-08-28 17:42:54 +0200

    List button files

--- addons/input/InputKey.gd
+++ addons/input/InputKey.gd
@@ -18,10 +18,20 @@ func _update() -> void:
 		var path = _create_path(key)
 		
 		print(path + " for " + str(key))
-		if not File.new().file_exists(path):
+		var dir = Directory.new()
+		dir.open(asset_folder)
+		dir.list_dir_begin()
+		
+		while true:
+			var file = dir.get_next()
+			if file == "":
+				break
+			elif not file.begins_with("."):
+				print(file)
+		
+		if not dir.file_exists(path):
 			print("Path does not exist. Using question mark")
 			path = _create_path(1)
-			_create_path(2)
 		
 		emit_signal("input_texture", path)
 	else:
